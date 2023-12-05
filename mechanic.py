7cca2c60a5a07bfd88586b08f2271127e7580018
jantektsan <yuniverse9999@hotmail.com>
2023-12-05 15:02:47 +0900

    getting links from Dental Practice site

--- mechanic.py
+++ mechanic.py
@@ -1,12 +1,22 @@
 import mechanicalsoup
 
 browser = mechanicalsoup.Browser()
-url = "http://olympus.realpython.org/login"
-login_page = browser.get(url)
-login_html = login_page.soup
+url = "https://ctc-associates.com/dental-practices-for-sale"
+page = browser.get(url)
+li_elements = page.soup.select("li.directory_link")
 
-form = login_html.select("form")[0]
-form.select("input")[0]["value"] = "zeus"
-form.select("input")[1]["value"] = "ThunderDude"
+for li_element in li_elements:
+    a_tag = li_element.select("a")[0]
+    if a_tag:
+        href = a_tag["href"]
+        print(href)
 
-profile_page = browser.submit(form, login_page.url)
+# url = "http://olympus.realpython.org/login"
+# login_page = browser.get(url)
+# login_html = login_page.soup
+
+# form = login_html.select("form")[0]
+# form.select("input")[0]["value"] = "zeus"
+# form.select("input")[1]["value"] = "ThunderDude"
+
+# profile_page = browser.submit(form, login_page.url)
